501b708cb893779ccf187efb07376d874954761b
CallaLittle <student@SHC-GEOLIB-003.shc.wisc.edu>
2016-03-28 18:28:32 -0500

    Module 8 assignment completed
    States drawn and projected. Website moved into bootstrap for responsive
    design

--- js/main.js
+++ js/main.js
@@ -3,26 +3,30 @@
 //global variables
 var csvData = 'data/studentDebtRenamedFields.csv';
 var geoData = 'data/50Reshaped.topojson';
-var width = 960;
-var height = 460;
+var width = 730;
+var height = 470;
 
 
 window.onload = setMap();
 
 function setMap() {
 
-	var map = d3.select('body')
+	var map = d3.select('#content')
 		.append('svg')
-		.attr('class', map)
+		.attr('class', 'map')
 		.attr('width', width)
 		.attr('height', height);
 
-	var projection = d3.geo.albers()
-		.center([-98.555319, 39.808801])
-		.rotate([-2, 0, 0])
-		.parallels([33.471773, 44.031051])
-		.scale(2500)
-		.translate([width/2, height/2]);
+	// var projection = d3.geo.albers()
+	// 	.center([1.82, 34.51])
+	// 	.rotate([99.18, -5.45, 0])
+	// 	.parallels([33.471773, 44.031051])
+	// 	.scale(806.07)
+	// 	.translate([width/2, height/2]);
+
+	var projection = d3.geo.albersUsa()
+        .scale(1000)
+        .translate([width / 2, height / 2]);
 
 	var path = d3.geo.path()
         .projection(projection);
@@ -36,10 +40,16 @@ function setMap() {
 		var states = topojson.feature(geoData, geoData.objects.ne_50m_admin_1_states_provinces_lakes).features;
 		console.log(states);
 
-		var statesUS = map.append("path")
-            .datum(states)
-            .attr("class", "states")
+		var statesUS = map.selectAll('.states')
+            .data(states)
+            .enter()
+            .append("path")
+            .attr("class", function(d) {
+            	return d.properties.name;
+            })
             .attr("d", path);
+
+        console.log(states);
 	};
 };
 
