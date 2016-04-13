5be16a7c27f2990a07614bbe39a65afe973f26dd
CallaLittle <wwestbury@shc.wisc.edu>
2016-04-13 04:40:32 -0500

    Functional
    All functions working. Need to style

--- js/main.js
+++ js/main.js
@@ -19,6 +19,17 @@
         chartInnerHeight = chartHeight - topBottomPadding * 2,
         translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
 
+    var dataMin = 0;
+    var dataMax = 35000;
+
+	var yScale = d3.scale.linear()
+			.range([0, chartHeight])
+			.domain([dataMin, dataMax]);
+
+	var axisScale = d3.scale.linear()
+		.range([0, chartHeight])
+		.domain([dataMax, dataMin]);
+
 	var csvAttributeArray = ['average_debt', 'average_tuition', 'default_rate', 'five_year_tuition_change', 'median_income', 'percent_with_debt', 'unemployment_rate'];
 	var expressed = csvAttributeArray[0];
 
@@ -57,7 +68,7 @@
 
 			setChart(csvData, colorScale);
 
-			createDropdown();
+			createDropdown(csvData);
 
 		};
 	};
@@ -134,18 +145,24 @@
 
 	function drawStates(map, states, path, colorScale) {
 
-		//console.log(states);
 		var statesUS = map.selectAll('.states')
 	        .data(states)
 	        .enter()
 	        .append("path")
 	        .attr("class", function(d) {
-	            return d.properties.name;
+	            return 'states ' + d.properties.name;
 	        })
 	        .attr("d", path)
 	        .style('fill', function(d) {
 	        	return colorScale(d.properties[expressed]);
-	        });
+	        })
+	        .on('mouseover', function(d) {
+	        	highlight(d.properties.name, d.properties);
+	        })
+	        .on('mouseout', function(d) {
+	        	dehighlight(d.properties.name);
+	        })
+	        .on('mousemove', moveLabel);
 	};
 
 	function setChart(csvData, colorScale) {
@@ -162,11 +179,7 @@
 	        .attr("height", chartInnerHeight)
 	        .attr("transform", translate);
 
-		var yScale = d3.scale.linear()
-			.range([0, chartHeight])
-			.domain([0, 35000]);
-
-		var bars = chart.selectAll('.bars')
+		var bars = chart.selectAll('.bar')
 			.data(csvData)
 			.enter()
 			.append('rect')
@@ -174,66 +187,29 @@
 				if(isNaN(a[expressed])) {
 					return 1;
 				};
-				//console.log(a[expressed])
 				return b[expressed] - a[expressed];
 			})
 			.attr('class', function(d) {
-				return 'bars ' + d.State;
+				return 'bar ' + d.State;
 			})
 			.attr('width', chartInnerWidth/csvData.length - 1)
-			.attr('x', function(d, i) {
-				return i * (chartInnerWidth/csvData.length) + leftPadding;
+			.on('mouseover', function(d) {
+				highlight(d.State, d);
 			})
-			.attr('height', function(d) {
-				if(isNaN(d[expressed])) {
-					return 0;
-				};
-				return yScale(parseFloat(d[expressed]));
-			})
-			.attr('y', function(d) {
-				if(isNaN(d[expressed])) {
-					return 0;
-				};
-				return (chartHeight - yScale(parseFloat(d[expressed]))) + topBottomPadding;
-			})
-			.style('fill', function(d) {
-				return colorScale(d[expressed]);
-			});
+			.on('mouseout', function(d) {
+	        	dehighlight(d.State);
+	        })
+	        .on('mousemove', moveLabel);
 
-		var axisScale = d3.scale.linear()
-			.range([0, chartHeight])
-			.domain([35000, 0]);
-
-    	var yAxis = d3.svg.axis()
-	        .scale(axisScale)
-	        .orient("left");
-
-	    var axis = chart.append("g")
-	        .attr("class", "axis")
-	        .attr('transform', translate)
-	        .call(yAxis);
-
-	    // var chartTitle = chart.append("text")
-	    //     .attr("x", 130)
-	    //     .attr("y", 40)
-	    //     .attr("class", "chartTitle")
-	    //     .text(function() {
-	    //     	var expressedVariable = expressed.split('_');
-	    //     	return expressedVariable[0][0].toUpperCase() + expressedVariable[0].slice(1) + " student " + expressedVariable[1] + " in each state";
-	    // 	});
-
-	    var chartTitle = d3.select("#title")
-	        .attr("class", "chartTitle")
-	        .text(function() {
-	        	var expressedVariable = expressed.split('_');
-	        	return expressedVariable[0][0].toUpperCase() + expressedVariable[0].slice(1) + " student " + expressedVariable[1] + " by state";
-	    	});
+		updateChart(bars, csvData.length, colorScale);
 
 	};
 
-	function createDropdown() {
-		console.log('hu')
+	function createDropdown(csvData) {
 		var options = d3.select('.dropdown')
+			.on('change', function() {
+				changeAttribute(this.value, csvData);
+			})
 			.selectAll('options')
 			.data(csvAttributeArray)
 			.enter()
@@ -251,6 +227,168 @@
 
 			});
 	};
+
+	function updateAxis() {
+
+		console.log(dataMax)
+
+		axisScale = d3.scale.linear()
+			.range([0, chartHeight])
+			.domain([dataMax, dataMin]);
+
+    	var yAxis = d3.svg.axis()
+	        .scale(axisScale)
+	        .orient("left");
+
+		d3.select('.axis')
+			.remove();	        
+
+	    var axis = d3.select('.chart')
+	    	.append("g")
+	        .attr("class", "axis")
+	        .attr('transform', translate)
+	        .call(yAxis);
+
+	};
+
+	function changeAttribute(attribute, csvData) {
+
+		expressed = attribute;
+
+		var colorScale = makeColorScale(csvData);
+
+		var statesUS = d3.selectAll('.states')
+			.transition()
+			.duration(1000)
+	        .style('fill', function(d) {
+	        	return colorScale(d.properties[expressed]);
+	        });
+
+	    var bars = d3.selectAll('.bar')
+	    	.sort(function(a, b) {
+				if(isNaN(a[expressed])) {
+					return 1;
+				};
+				// console.log(a[expressed])
+				return b[expressed] - a[expressed];
+			})
+			.transition()
+			.delay(function(d, i) {
+				return i * 11;
+			})
+			.duration(250);
+
+	    dataMax = d3.max(csvData, function(d) { return parseFloat(d[expressed]); })
+
+	    updateChart(bars, csvData.length, colorScale);
+	};
+
+	function updateChart(bars, n, colorScale) {
+
+		var yScale = d3.scale.linear()
+			.range([0, chartHeight])
+			.domain([dataMin, dataMax]);
+
+		bars.attr('x', function(d, i) {
+				return i * (chartInnerWidth/n) + leftPadding;
+			})
+			.attr('height', function(d) {
+				if(isNaN(d[expressed])) {
+					return 0;
+				};
+				return yScale(parseFloat(d[expressed]));
+			})
+			.attr('y', function(d) {
+				if(isNaN(d[expressed])) {
+					return 0;
+				};
+				return (chartHeight - yScale(parseFloat(d[expressed]))) + topBottomPadding;
+			})
+			.style('fill', function(d) {
+				return colorScale(d[expressed]);
+			});
+
+		updateAxis();
+
+		var chartTitle = d3.select("#chartTitle")
+	        .text(function() {
+				var expressedVariable = expressed.split('_');
+				var optionText = expressedVariable[0][0].toUpperCase() + expressedVariable[0].slice(1);
+				for(var i = 1; i < expressedVariable.length; i++) {
+					optionText += " " + expressedVariable[i];
+				}
+				return optionText;
+
+			});
+	};
+
+
+function setLabel(state, props) {
+
+		var expressedVariable = expressed.split('_');
+		state = state.replace('_', ' ');
+		var labelAttribute = '<h4>' + state + ' '+ expressedVariable[0][0].toUpperCase() + expressedVariable[0].slice(1);
+
+		for(var i = 1; i < expressedVariable.length; i++) {
+			labelAttribute += " " + expressedVariable[i][0].toUpperCase() + expressedVariable[i].slice(1);
+		};
+
+		labelAttribute  += '</h4><p>' + props[expressed] + '</p>';
+
+		var infolabel = d3.select('#content')
+			.append('div')
+			.attr({
+				'class': 'infolabel',
+				'id': state + '_label'
+			})
+			.html(labelAttribute);
+
+	};
+	function highlight(state, props) {
+		var selected = d3.selectAll('.' + state)
+			.style({
+				'stroke': '#00c24e',
+				'stroke-width': '3'
+			});
+
+		setLabel(state, props);
+	};
+
+	function dehighlight(state) {
+		var selected = d3.selectAll('.' + state)
+			.style({
+				'stroke': '#e5e5e5',
+				'stroke-width': '.5px'
+			});
+
+		d3.select('.infolabel')
+			.remove();
+	};
+
+	function moveLabel() {
+
+		var labelWidth = d3.select('.infolabel')
+			.node()
+			.getBoundingClientRect()
+			.width;
+
+		var x1 = d3.event.clientX + 10;
+		var y1 = d3.event.clientY - 75;
+		var x2 = d3.event.clientX - labelWidth - 10;
+		var y2 = d3.event.clientY + 25;
+
+		var x = d3.event.clientX > window.innerWidth - labelWidth - 20 ? x2 : x1;
+
+		var y = d3.event.clientY < 75 ? y2 : y1; 
+
+		d3.select('.infolabel') 
+			.style({
+				'left': x + 'px',
+				'top': y + 'px'
+			});
+	}
+
+
 
 })();
 
